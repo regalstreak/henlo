d5eeddb16e34d408520e726225e90a8a9dfbfd21
Neil Agarwal <neil@neilagarwal.me>
2019-08-09 20:42:24 +0530

    henlo: react-native-contacts

--- android/app/src/main/java/com/henlo/MainApplication.java
+++ android/app/src/main/java/com/henlo/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -25,8 +27,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
       // packages.add(new MyReactNativePackage());
+
+      packages.add(new ReactNativeContacts());
+      
       return packages;
     }
 
